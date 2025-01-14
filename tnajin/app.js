1f757a7f590781b7ab1fb9dda106dca1e1f1cde1
Meline2205 <melikyanmeline22@outlook.com>
2025-01-14 23:58:59 +0400

    Add files via upload

---
+++ tnajin/app.js
@@ -0,0 +1,19 @@
+// var qash = prompt();
+// var kg = 300;
+// var gin = 0;
+// gin = qash * kg;
+// document.write(gin, "dram")
+// var qash = prompt()
+// var jur = 0.03;
+// var l = 0;
+// l = qash * jur;
+// document.write(l)
+var rub = 3.83;
+var usd = 394;
+var eur = 402;
+var dram = prompt("Դրամ");
+var rub = dram / rub;
+var usd = dram / usd;
+var eur = dram / eur;
+document.write("RUB`"  + rub + "<br>" + "USD`"  + usd + "<br>" + "EUR`"  + eur + "<br>")
+
