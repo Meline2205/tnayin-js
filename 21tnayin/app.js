e82c4c9ba017666fc5b6606709949e112ab657c8
Meline2205 <melikyanmeline22@outlook.com>
2025-01-22 14:12:11 +0400

    Add files via upload

---
+++ 21tnayin/app.js
@@ -0,0 +1,15 @@
+// var b = prompt("greq tiv")
+// for( var i = 0; i <= b; i++){
+//     console.log(i)
+// }
+// var h = prompt()
+// for( var i = h; i >= 1; i--){
+//     console.log(i)
+// }
+for(var i = 1; i <= 10; i++){
+    if(i % 2==1){
+        document.write("<h1>hello world</h1>")
+    }else{
+        document.write("<h3>hello world</h3>")
+    }
+}
